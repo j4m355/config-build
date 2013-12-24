ec67196e237c5b685152d60086eedb394fbb2d99
j4m355 <e@mailjames.com>
2013-12-24 15:26:51 +0000

    create base folder structure

---
+++ README.md
@@ -0,0 +1,8 @@
+    rake onboard:newApp[appName]
+
+produces the folder structure:
+    appName
+    	base-config
+    	settings.json
+
+
--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,17 @@
-namespace :create do
 
+namespace :onboard do
+	
 	desc "add new app to generate configs for"
-	task :newApp do => :environment |task, args|
+	task :newApp, [:appName] do |t, args|
 		Dir.chdir "working-dir" do 
-			FileUtils.mkdir_p(args[0])
+			FileUtils.mkdir_p(args.appName)
+			Dir.chdir args.appName do 
+				FileUtils.mkdir_p("base-config")
+				File.open("settings.json", 'w') {|f| f.write("{\n\"appName\" : \"" + args.appName + "\"\n}") }
+			end
 		end
 	end	
+
 
 	
 end
